0236077d2fb1fe14bb840ced276b5d14a5beaca2
GonzaloRojas <gonzalo.edurojas@gmail.com>
2025-01-14 20:27:21 -0300

    funcion para renderizar grafico

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 
 
-async function convertirMoneda(){
+async function convertirMoneda() {
     const dinero = document.getElementById('dinero').value;
     const moneda = document.getElementById('moneda').value;
     const resultado = document.getElementById('resultado');
-    try{
+    try {
         const response = await fetch("https://mindicador.cl/api/");
         const data = await response.json();
-        //console.log(data);
-        
-        let montoCambio = ""; 
+        console.log(data);
+
+        let montoCambio = "";
         if (moneda === 'dolar') {
             montoCambio = data.dolar.valor
         }
@@ -19,21 +19,78 @@ async function convertirMoneda(){
         else if (moneda === 'uf') {
             montoCambio = data.uf.valor
         }
-        else if (moneda === 'utm'){
-            montoCambio =data.utm.valor
+        else if (moneda === 'utm') {
+            montoCambio = data.utm.valor
         }
 
         //console.log("Valor del cambio: ", montoCambio);
-        
-        const conversion = ( dinero / montoCambio ).toFixed(2);
+
+        const conversion = (dinero / montoCambio).toFixed(2);
         //console.log(conversion);
-        resultado.innerHTML = ` ${conversion} ${moneda} `
-        
+        resultado.innerText = ` ${conversion} ${moneda} `
+
+        //Agregar la funcion para renderizar grafico
+        renderChart(moneda)
     }
 
-    catch(error) {
+    catch (error) {
         console.log('error: ', error);
-        
+
     }
 }
 
+//Funcion para renderizar el gráfico con datos historicos
+let grafico = '';
+
+async function renderChart(moneda) {
+    try {
+        const response = await fetch(`https://mindicador.cl/api/${moneda}`);
+        const data = await response.json();
+        //console.log(data);
+
+        //extraer informacion de los últimos 10 días
+        const historicData = data.serie.slice(0,10).reverse()
+        const etiquetas = historicData.map( item => item.fecha.slice(0, 10));
+        const valores = historicData.map( item => item.valor);
+
+        if (grafico) {
+            //Elimina un grafico existente, si existe
+            grafico.destroy()
+        }
+        const context = document.getElementById('grafico').getContext("2d");
+
+        grafico = new Chart( context, {
+            type: 'line',
+            data: {
+                labels: etiquetas,
+                datasets: [{
+                    label: `Valor en los últimos 10 días (${moneda})`,
+                    data: valores,
+                    borderColor: "#ff0000",
+                    tension: 0.1
+
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                scales: {
+                    x: {
+                        ticks: {
+                            maxRotation: 45,
+                            minRotation: 0
+                        }
+                    }
+                }
+            }
+        })
+
+    }
+
+    catch (error) {
+        console.log("Error:", error);
+
+    }
+}
+
+renderChart(moneda)
